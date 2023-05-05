d9bf8efe8c8dd85cd51313ff3a7cf3f412561be7
theboyulove <phuhieupham4@gmail.com>
2023-05-05 23:53:08 +0700

    add cheerio

--- netlify/functions/news2.js
+++ netlify/functions/news2.js
@@ -25,8 +25,7 @@ exports.handler = async (event, context) => {
   $('div.entry-content img').each((index, element) => {
     const dataSrc = $(element).attr('data-src');
     if (dataSrc) {
-      $(element).attr('src', dataSrc);
-      $(element).removeAttr('data-src');
+      $(element).attr('src', dataSrc).removeAttr('data-src');
     }
   });
 

215ada922d52ab1c59b4ec1de8528e60ccfccb6c
theboyulove <phuhieupham4@gmail.com>
2023-05-06 02:48:41 +0700

    change folder

--- pages/demo/article.js
+++ pages/demo/article.js
@@ -6,12 +6,10 @@ function Article() {
   useEffect(() => {
     const fetchArticle = async () => {
       try {
-        const search = window.location.search;
-        const params = new URLSearchParams(search);
-        const id = params.get("id");
+        const id = new URLSearchParams(window.location.search).get("id");
         const response = await fetch(`/.netlify/functions/news2?id=${id}`);
-        const data = await response.json();
-        setArticle(data);
+        const html = await response.text();
+        setArticle(html);
       } catch (error) {
         console.error(error);
       }
@@ -20,36 +18,31 @@ function Article() {
     fetchArticle();
   }, []);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const lazyImage = entry.target;
-          const dataSrc = lazyImage.getAttribute("data-src");
-          if (dataSrc) {
-            lazyImage.setAttribute("src", dataSrc);
-            lazyImage.removeAttribute("data-src");
-            lazyImage.setAttribute("loading", "lazy");
-          }
-          observer.unobserve(lazyImage);
-        }
-      });
-    });
-    document.querySelectorAll('img[data-src]').forEach((img) => {
-      observer.observe(img);
-    });
-  }, [article]);
-
   if (!article) {
     return <div>Loading article...</div>;
   }
 
   return (
-    <div>
-      <h1>{article.title}</h1>
-      <img data-src={article.image} alt={article.title} />
-      <p>{article.content}</p>
-    </div>
+    <div dangerouslySetInnerHTML={{ __html: article }}></div>
+    <script>
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const lazyImage = entry.target;
+            const dataSrc = lazyImage.getAttribute('data-src');
+            if (dataSrc) {
+              lazyImage.setAttribute('src', dataSrc);
+              lazyImage.removeAttribute('data-src');
+              lazyImage.setAttribute('loading', 'lazy');
+            }
+            observer.unobserve(lazyImage);
+          }
+        });
+      });
+      document.querySelectorAll('img[data-src]').forEach((img) => {
+        observer.observe(img);
+      });
+    </script>
   );
 }
 

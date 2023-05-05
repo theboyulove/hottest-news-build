4ae0ff2e4bfa61b0a973007499057fc9a08b487d
theboyulove <phuhieupham4@gmail.com>
2023-05-06 02:39:50 +0700

    add cheerio

--- pages/demo/article.js
+++ pages/demo/article.js
@@ -6,13 +6,12 @@ function Article() {
   useEffect(() => {
     const fetchArticle = async () => {
       try {
-        const urlParams = new URLSearchParams(window.location.search);
-        const articleId = urlParams.get("id");
-        const response = await fetch(`/.netlify/functions/news2?id=${articleId}`);
-        const data = await response.text();
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, "text/html");
-        setArticle(doc);
+        const search = window.location.search;
+        const params = new URLSearchParams(search);
+        const id = params.get("id");
+        const response = await fetch(`/.netlify/functions/news2?id=${id}`);
+        const data = await response.json();
+        setArticle(data);
       } catch (error) {
         console.error(error);
       }
@@ -21,35 +20,35 @@ function Article() {
     fetchArticle();
   }, []);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const lazyImage = entry.target;
+          const dataSrc = lazyImage.getAttribute("data-src");
+          if (dataSrc) {
+            lazyImage.setAttribute("src", dataSrc);
+            lazyImage.removeAttribute("data-src");
+            lazyImage.setAttribute("loading", "lazy");
+          }
+          observer.unobserve(lazyImage);
+        }
+      });
+    });
+    document.querySelectorAll('img[data-src]').forEach((img) => {
+      observer.observe(img);
+    });
+  }, [article]);
+
   if (!article) {
     return <div>Loading article...</div>;
   }
 
   return (
     <div>
-      <h1>{article.querySelector("h1").textContent}</h1>
-      {article.querySelectorAll("img").length > 0 &&
-        <script>
-          const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                const lazyImage = entry.target;
-                const dataSrc = lazyImage.getAttribute('data-src');
-                if (dataSrc) {
-                  lazyImage.setAttribute('src', dataSrc);
-                  lazyImage.removeAttribute('data-src');
-                  lazyImage.setAttribute('loading', 'lazy');
-                }
-                observer.unobserve(lazyImage);
-              }
-            });
-          });
-          document.querySelectorAll('img[data-src]').forEach((img) => {
-            observer.observe(img);
-          });
-        </script>
-      }
-      <div dangerouslySetInnerHTML={{ __html: article.body.innerHTML }}></div>
+      <h1>{article.title}</h1>
+      <img data-src={article.image} alt={article.title} />
+      <p>{article.content}</p>
     </div>
   );
 }

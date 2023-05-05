ee85bfe5f34285c2ffb532437d58431e68cd969e
theboyulove <phuhieupham4@gmail.com>
2023-05-06 00:23:58 +0700

    change folder

--- netlify/functions/news2.js
+++ netlify/functions/news2.js
@@ -21,13 +21,26 @@ exports.handler = async (event, context) => {
   // Get the featured image of the article
   const featuredImageUrl = $('div.entry-content img').first().attr('data-src');
 
-  // Replace the src attributes of all images with their corresponding data-src attributes
+  // Replace the src attributes of all images with their corresponding data-src attributes and add lazy-loading attribute
   $('img[data-src]').each((index, element) => {
     const dataSrc = $(element).attr('data-src');
     if (dataSrc) {
-      $(element).attr('src', dataSrc).removeAttr('data-src');
+      $(element).attr('src', dataSrc).removeAttr('data-src').attr('loading', 'lazy');
     }
   });
+
+  // Add header menu
+  const headerMenu = `
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#home"><strong>Home</strong></a></li>
+          <li><a href="#news"><strong>News</strong></a></li>
+          <li><a href="#contact"><strong>Contact</strong></a></li>
+        </ul>
+      </nav>
+    </header>
+  `;
 
   // Format the HTML output using Prettier
   const formattedHtml = prettier.format(
@@ -37,8 +50,9 @@ exports.handler = async (event, context) => {
           <title>${title}</title>
         </head>
         <body>
+          ${headerMenu}
           <h1>${title}</h1>
-          <img src="${featuredImageUrl}">
+          <img src="${featuredImageUrl}" loading="lazy">
           <div>${articleContent}</div>
         </body>
       </html>

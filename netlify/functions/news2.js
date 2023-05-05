d67306eaca9244a5f0ce283e3717bd965a50a1ec
theboyulove <phuhieupham4@gmail.com>
2023-05-05 22:54:02 +0700

    add cheerio

---
+++ netlify/functions/news2.js
@@ -0,0 +1,53 @@
+const cheerio = require('cheerio');
+const prettier = require('prettier');
+
+exports.handler = async (event, context) => {
+  const articleId = event.queryStringParameters.id;
+  const articleUrl = `https://aubtu.biz/${articleId}`;
+
+  const { default: fetch } = await import('node-fetch');
+
+  const response = await fetch(articleUrl);
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+
+  // Get the title of the article
+  const title = $('h1.entry-title').text();
+
+  // Get the main content of the article
+  const articleContent = $('div.entry-content').html();
+
+  // Get the featured image of the article
+  let featuredImageUrl = $('div.entry-content img').first().attr('src');
+
+  // Replace the data URL with the original image URL if it exists
+  if (featuredImageUrl && featuredImageUrl.startsWith('data:')) {
+    featuredImageUrl = $('div.entry-content img').first().attr('data-src');
+  }
+
+  // Format the HTML output using Prettier
+  const formattedHtml = prettier.format(
+    `
+      <html>
+        <head>
+          <title>${title}</title>
+        </head>
+        <body>
+          <h1>${title}</h1>
+          <img src="${featuredImageUrl}">
+          <div>${articleContent}</div>
+        </body>
+      </html>
+    `,
+    { parser: 'html' }
+  );
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: formattedHtml,
+  };
+};

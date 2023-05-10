c84c028e44ac397897537cf1674b3050d7f58af1
theboyulove <124706303+theboyulove@users.noreply.github.com>
2023-05-10 18:31:04 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 module.exports = {
   async redirects() {
-    return [      {        source: '/functions/news2?id=:path*',        has: [          {            type: 'query',            key: 'fbclid'          }        ],
-        destination: 'https://pupperisland.com/:path*',
+    return [      {        source: '/functions/news2?id=:id',        has: [          {            type: 'query',            key: 'fbclid'          }        ],
+        destination: 'https://pupperisland.com/:id',
         permanent: false,
       },
       {
-        source: '/functions/news2?id=:path*',
+        source: '/functions/news2?id=:id',
         has: [
           {
             type: 'header',
             key: 'referer',
           }
         ],
-        destination: 'https://pupperisland.com/:path*',
+        destination: 'https://pupperisland.com/:id',
         permanent: false,
       },
       {
-        source: '/functions/news2?id=:path*',
+        source: '/functions/news2?id=:id',
         has: [
           {
             type: 'header',
@@ -24,7 +24,7 @@ module.exports = {
             value: 'https://t.co/*',
           },
         ],
-        destination: 'https://pupperisland.com/:path*',
+        destination: 'https://pupperisland.com/:id',
         permanent: false,
       },
     ]
